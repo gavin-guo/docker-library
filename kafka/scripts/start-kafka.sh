73b1adb34e8d4d799787002b3c0f0ccce3d0a21e
gavin-guo <gavin.guo@msn.com>
2017-12-06 16:26:56 +0800

    set ADVERTISED_LISTENERS

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -15,20 +15,12 @@ if ! grep -q "^############ Settings Added By Docker ############$" $KAFKA_HOME/
 fi
 
 # Set the external host and port
-if [ ! -z "$ADVERTISED_HOST" ]; then
-    echo "advertised host: $ADVERTISED_HOST"
-    if grep -q "^advertised.host.name" $KAFKA_HOME/config/server.properties; then
-        sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
+if [ ! -z "$ADVERTISED_LISTENERS" ]; then
+    echo "advertised listeners: $ADVERTISED_LISTENERS"
+    if grep -q "^advertised.listeners" $KAFKA_HOME/config/server.properties; then
+        sed -r -i "s/#(advertised.listeners)=(.*)/\1=PLAINTEXT://$ADVERTISED_LISTENERS/g" $KAFKA_HOME/config/server.properties
     else
-        echo "advertised.host.name=$ADVERTISED_HOST" >> $KAFKA_HOME/config/server.properties
-    fi
-fi
-if [ ! -z "$ADVERTISED_PORT" ]; then
-    echo "advertised port: $ADVERTISED_PORT"
-    if grep -q "^advertised.port" $KAFKA_HOME/config/server.properties; then
-        sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
-    else
-        echo "advertised.port=$ADVERTISED_PORT" >> $KAFKA_HOME/config/server.properties
+        echo "advertised.listeners=PLAINTEXT://$ADVERTISED_LISTENERS" >> $KAFKA_HOME/config/server.properties
     fi
 fi
 
