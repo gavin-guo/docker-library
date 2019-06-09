adc3774a0f097372eb8f567da8a9eb6bd4a55d55
gavin-guo <gavin.guo@msn.com>
2019-06-09 15:53:47 +0800

    no message

--- kafka-with-zookeeper/scripts/start-kafka-offset-monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-java -cp /kafkaOffsetMonitor.jar com.quantifind.kafka.offsetapp.OffsetGetterWeb \
---offsetStorage kafka \
---kafkaBrokers localhost:9092 \
---zk localhost:2181 \
---port ${KAFKA_MONITOR_PORT} \
---refresh 10.seconds \
---retain 2.days
