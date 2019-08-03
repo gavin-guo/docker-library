9a72a6ab6e02d16b3f56727e8ddf7d8c7069d3af
gavin-guo <gavin.guo@msn.com>
2019-08-03 11:59:42 +0800

    no message

--- kafka_zk-built-in/docker-entrypoint.sh
+++ kafka_zk-built-in/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# for zookeeper
 if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
   CONFIG="$ZOO_CONF_DIR/zoo.cfg"
 
@@ -21,9 +22,11 @@ if [[ ! -f "$ZOO_DATA_DIR/myid" ]]; then
   echo "${ZOO_MY_ID:-1}" >"$ZOO_DATA_DIR/myid"
 fi
 
+# for kafka
 sed -r -i "s/#(listeners)=(.*)/\1=PLAINTEXT:\/\/0.0.0.0:9092/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/#(advertised.listeners)=(.*)/\1=PLAINTEXT:\/\/0.0.0.0:9092/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/(zookeeper.connect)=(.*)/\1=127.0.0.1:$ZOO_PORT/g" $KAFKA_HOME/config/server.properties
+sed -r -i "s/(log.dirs)=(.*)/\1=$KAFKA_LOG_DIR/g" $KAFKA_HOME/config/server.properties
 
 if [ ! -z "$LOG_RETENTION_HOURS" ]; then
   echo "log.retention.hours: $LOG_RETENTION_HOURS"
